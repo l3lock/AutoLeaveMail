83bf8552035094cfbb71869e2ffa94212360d5be
Thawinwats Uggaravisee <thawinwats@sirimedia.com>
2018-03-30 11:03:22 +0700

    Adding avatar

---
+++ src/components/Avatar.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-layout align-center column>
+    <v-avatar 
+      size="135px" 
+      class="my-4"
+      slot="avatar"
+      > 
+        <img :src="avatar">
+    </v-avatar>
+    <h2>{{ name }}</h2>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'avatar',
+  data() {
+    return {
+      avatar: require('@/assets/chris.jpg'),
+      name: 'Thawinwats Uggaravisee'
+    }
+  }
+}
+</script>
+
