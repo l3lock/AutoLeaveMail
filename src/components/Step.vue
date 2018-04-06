60a543f35ed376c21e2f6d296772fad4c55d2e96
Thawinwats Uggaravisee <111.pk42@gmail.com>
2018-04-06 12:11:07 +0700

    Choose a date to leave step

--- src/components/Step.vue
+++ src/components/Step.vue
@@ -11,13 +11,24 @@
       <v-btn color="primary ml-0" @click.native="steps = 2">Continue</v-btn>
       <v-btn flat @click="clearSelectTypeOfLeave">Cancel</v-btn>
     </v-stepper-content>
-    <v-stepper-step step="2" :complete="steps > 2" :editable="steps > 2">Configure analytics for this app</v-stepper-step>
+
+    <v-stepper-step step="2" :complete="steps > 2" :editable="steps > 2">
+      Choose a date to leave
+    </v-stepper-step>
     <v-stepper-content step="2">
-      <v-card color="grey lighten-1" class="mb-4" height="200px"></v-card>
+
+      <v-flex xs12 class="mb-4">
+        <v-card color="grey darken-2">
+          <v-card-text>
+            <DateRange :options="dateRangeOptions"/>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+
       <v-btn color="primary ml-0" @click.native="steps = 3">Continue</v-btn>
       <v-btn flat>Cancel</v-btn>
     </v-stepper-content>
-    <v-stepper-step step="3" :complete="steps > 3" :editable="steps > 3">Select an ad format and name ad unit</v-stepper-step>
+    <!-- <v-stepper-step step="3" :complete="steps > 3" :editable="steps > 3">Select an ad format and name ad unit</v-stepper-step>
     <v-stepper-content step="3">
       <v-card color="grey lighten-1" class="mb-4" height="200px"></v-card>
       <v-btn color="primary ml-0" @click.native="steps = 4">Continue</v-btn>
@@ -28,27 +39,37 @@
       <v-card color="grey lighten-1" class="mb-4" height="200px"></v-card>
       <v-btn color="primary ml-0" @click.native="steps = 1">Continue</v-btn>
       <v-btn flat>Cancel</v-btn>
-    </v-stepper-content>
+    </v-stepper-content> -->
   </v-stepper>
 </template>
 
 <script>
   import { mapState, mapActions } from 'vuex'
+
   import LeaveType from '@/components/LeaveType'
+  import DateRange from '@/components/DateRange'
+
   export default {
     data () {
       return {
-        steps: 1
+        steps: 1,
+        dateRangeOptions: {
+          startDate: new Date(),
+          endDate: new Date(),
+          format: 'DD/MM/YYYY',
+          chipFormat: 'DD-MM-YYYY',
+        },
       }
     },
     components: {
-      LeaveType
+      LeaveType,
+      DateRange
     },
     methods: {
       // handleClearSelectTypeOfLeave () {
       //   this.$store.dispatch('clearSelectTypeOfLeave')
       // },
-      ...mapActions(['clearSelectTypeOfLeave'])
+      ...mapActions(['clearSelectTypeOfLeave']),
     }
   }
 </script>
